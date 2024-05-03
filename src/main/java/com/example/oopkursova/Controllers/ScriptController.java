8cd004407b6171da35de39254ffa1a333b73135a
lion2 <10328036@stud.op.edu.ua>
2024-05-03 10:46:16 +0200

    Commit 9(update 3)

--- src/main/java/com/example/oopkursova/Controllers/ScriptController.java
+++ src/main/java/com/example/oopkursova/Controllers/ScriptController.java
@@ -3,24 +3,26 @@ package com.example.oopkursova.Controllers;
 import com.example.oopkursova.Entity.Script;
 import com.example.oopkursova.Loggable;
 import com.example.oopkursova.Repository.ScriptRepo;
+import com.example.oopkursova.Service.ScriptService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class ScriptController {
 
     private final ScriptRepo scriptRepo;
+    private final ScriptService service;
     private static final Logger logger = LoggerFactory.getLogger(ScriptController.class);
 
 
-    public ScriptController(ScriptRepo scriptRepo) {
+    public ScriptController(ScriptRepo scriptRepo, ScriptService service) {
         this.scriptRepo = scriptRepo;
+        this.service = service;
     }
     @Loggable
     @GetMapping("/CreatingScriptMovie")
@@ -33,6 +35,18 @@ public class ScriptController {
     public String createScriptMovie(@Valid Script script){
         scriptRepo.save(script);
         logger.info("Script added to movie: {}", script);
+        return "MenuDirectors";
+    }
+
+    @GetMapping("/edit_script")
+    public String showUpdateForm(Model model) {
+        model.addAttribute("script", new Script());
+        return "UpdateScriptFilms";
+    }
+
+    @PostMapping("/edit_script")
+    public String updateScript(@RequestParam("id") Long id,@ModelAttribute Script updatedScript) {
+        service.update(id,updatedScript);
         return "MenuDirectors";
     }
 
---
+++ src/main/java/com/example/oopkursova/Service/ScriptService.java
@@ -0,0 +1,34 @@
+package com.example.oopkursova.Service;
+
+import com.example.oopkursova.Entity.Script;
+import com.example.oopkursova.Repository.ScriptRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ScriptService {
+
+    @Autowired
+    private final ScriptRepo scriptRepo;
+
+    public ScriptService(ScriptRepo scriptRepo) {
+        this.scriptRepo = scriptRepo;
+    }
+
+    public Script findById(Long id) {
+        Optional<Script> scriptOptional = scriptRepo.findById(id);
+        return scriptOptional.orElseThrow(() -> new RuntimeException("Script with id " + id + " not found"));
+    }
+
+    public void update(Long id ,Script updatedScript) {
+        Script script = scriptRepo.findById(id).
+                orElseThrow(() -> new RuntimeException("Script with id "+ id + "not found"));
+        Script existingScript = findById(updatedScript.getId());
+        existingScript.setContent(updatedScript.getContent());
+        scriptRepo.save(existingScript);
+    }
+
+
+}

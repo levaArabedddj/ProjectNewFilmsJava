18f7fd1adb65bef7e2564dfc72fbd2878e5bd9e3
lion2 <10328036@stud.op.edu.ua>
2025-02-23 17:02:06 +0100

    Update project , part 2

---
+++ src/main/java/com/example/oopkursova/Entity/Actor_Profiles.java
@@ -0,0 +1,17 @@
+package com.example.oopkursova.Entity;
+
+public class Actor_Profiles {
+    // создать новую сущность
+    // для актеров , куда они будут добавлять в свои профили
+    // свои возможности и умения , по тибу своего личного кабинета для актера
+    // вот пример кода скл
+    // CREATE TABLE actor_profiles (
+    //    id BIGSERIAL PRIMARY KEY,
+    //    actor_id BIGINT UNIQUE NOT NULL,
+    //    bio TEXT,
+    //    skills TEXT, -- JSON або CSV (наприклад, "Фехтування, Танці, Вокал")
+    //    experience TEXT, -- Опис досвіду
+    //    profile_photo_url VARCHAR(500), -- Посилання на фото
+    //    FOREIGN KEY (actor_id) REFERENCES users(user_id)
+    //);
+}

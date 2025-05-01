10ccf8524d71c51d65eab411bb65c1f66b3c6f42
lion2 <10328036@stud.op.edu.ua>
2025-05-01 16:44:55 +0200

    New controllers elastic search

--- src/main/java/com/example/ElasticSearch/ElasticSearchController.java
+++ src/main/java/com/example/ElasticSearch/ElasticSearchController.java
@@ -2,14 +2,17 @@ package com.example.ElasticSearch;
 
 import com.example.ElasticSearch.ClassDocuments.ActorDocument;
 import com.example.ElasticSearch.ClassDocuments.CrewMemberDocument;
+import com.example.ElasticSearch.ClassDocuments.MovieDocument;
 import com.example.ElasticSearch.Service.ActorService;
 
 import com.example.ElasticSearch.Service.CrewMemberServiceElastic;
+import com.example.ElasticSearch.Service.MovieElasticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,9 +24,12 @@ public class ElasticSearchController {
     private final CrewMemberServiceElastic crewMemberService;
 
     @Autowired
-    public ElasticSearchController(ActorService actorService, CrewMemberServiceElastic crewMemberService) {
+    private final MovieElasticService elasticService;
+    @Autowired
+    public ElasticSearchController(ActorService actorService, CrewMemberServiceElastic crewMemberService, MovieElasticService elasticService) {
         this.actorService = actorService;
         this.crewMemberService = crewMemberService;
+        this.elasticService = elasticService;
     }
 
 
@@ -57,4 +63,51 @@ public class ElasticSearchController {
         return ResponseEntity.ok(crewMemberDocuments);
     }
 
+
+
+
+     //1. Поиск фильмов по точному названию
+
+    @GetMapping("/title")
+    public ResponseEntity<List<MovieDocument>> getByTitle(
+            @RequestParam String title,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        try {
+            List<MovieDocument> movies = elasticService.getMovieByTitle(title, size);
+            return ResponseEntity.ok(movies);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+
+//     2.Поиск фильмов по жанру
+
+    @GetMapping("/genre/{genre}")
+    public ResponseEntity<List<MovieDocument>> getByGenre(@PathVariable String genre) throws IOException {
+        List<MovieDocument> list = elasticService.getGenreBy(genre);
+        return ResponseEntity.ok(list);
+    }
+
+
+     //     3. Полнотекстовый поиск в описании
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieDocument>> fullTextSearch(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) String genre,
+            @RequestParam(required = false) String text,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size
+    ) throws IOException {
+        if (title != null && genre != null && text != null) {
+            List<MovieDocument> list = elasticService.complexSearch(title, genre, text, from, size);
+            return ResponseEntity.ok(list);
+        }
+
+        return ResponseEntity.ok(List.of());
+    }
+
+
 }
+
--- src/main/java/com/example/ElasticSearch/Service/MovieElasticService.java
+++ src/main/java/com/example/ElasticSearch/Service/MovieElasticService.java
@@ -1,16 +1,42 @@
 package com.example.ElasticSearch.Service;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.CompletionSuggestOption;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.Suggestion;
 import com.example.ElasticSearch.ClassDocuments.MovieDocument;
 
 import com.example.Entity.Movies;
+
+import org.elasticsearch.common.unit.Fuzziness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.util.stream.Collectors;
+
 
 @Service
 public class MovieElasticService {
+
+    @Autowired
+    private final ElasticsearchClient client;
+    private final String INDEX = "movies";
+
+    public MovieElasticService(ElasticsearchClient client) {
+        this.client = client;
+    }
+
+
     public MovieDocument mapToElastic(Movies movie){
         MovieDocument document = new MovieDocument();
         document.setId(String.valueOf(movie.getId()));
@@ -19,4 +45,68 @@ public class MovieElasticService {
         document.setGenre_film(movie.getGenre_film().name());
         return document;
     }
+
+    public List<MovieDocument> getMovieByTitle(String title, int size) throws IOException {
+
+        SearchResponse<MovieDocument> resp = client.search(s -> s
+                        .index(INDEX)
+                        .size(size)
+                        .query(q -> q
+                                .match(m -> m
+                                        .field("title")
+                                        .query(title)
+                                        .fuzziness("AUTO")
+                                        .maxExpansions(50)
+                                        .prefixLength(1)
+                                )
+                        ),
+                MovieDocument.class
+        );
+
+        return resp.hits().hits().stream()
+                .map(Hit::source).toList();
+    }
+
+    public List<MovieDocument> getGenreBy(String genre) throws IOException {
+
+    var response = client.search(
+             s -> s.index(INDEX).
+                     query( q -> q.term(
+                             t -> t.field("genre_film.keyword")
+                                     .value( v -> v.stringValue(genre))
+                     )
+                     ),
+            MovieDocument.class);
+
+    return response.hits().hits()
+            .stream()
+            .map(h -> h.source()).toList();
+    }
+
+    public List<MovieDocument> complexSearch(String title, String genre, String description, int from, int size ) throws IOException {
+
+        var response = client.search(s -> s.index(INDEX)
+                        .from(from)
+                        .size(size)
+                        .query(q ->
+                                q.bool(b ->
+                                        b.must(m1 -> m1. match(m -> m. field("title"). query(title).fuzziness("AUTO").maxExpansions(50).prefixLength(1)))
+                                                .filter(f -> f. term(t -> t. field("genre_film.keyword"). value(v -> v.stringValue(genre))))
+                                                .must(m2 -> m2. match(m -> m. field("description"). query(description).fuzziness("AUTO").maxExpansions(50).prefixLength(1)))
+                                )).
+                        sort(
+                                st -> st. field(f -> f. field("title.keyword"). order(SortOrder.Asc))
+                        ),
+
+
+                MovieDocument.class);
+
+        return response.hits().hits().stream().map( h -> h.source()).toList();
+    }
+
+
+
+
+
+
 }
